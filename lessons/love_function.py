76d403909bb4634e1b68967b8a7c7652cae8e8ea
Qingwei Li <qingwei.li@unc.edu>
2022-02-05 12:04:35 -0500

    ex03 1st

---
+++ lessons/love_function.py
@@ -0,0 +1,13 @@
+def love(name: str) -> str:
+    """Given a name as a parameter, returns a loving string."""
+    return f"I love you {name}!"
+
+
+def spread_love(to: str, n: int) -> str:
+    """Generates a string that repeats a loving message n times."""
+    love_note = ""
+    counter = 0
+    while counter < n:
+        love_note = love_note + love(to) + "\n"
+        counter += 1
+    return love_note
